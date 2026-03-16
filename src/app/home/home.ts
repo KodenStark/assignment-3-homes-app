a6251c1b479218abf8b1c51659294f2804ab99f1
Koden Stark <legacykid.7653@gmail.com>
2026-03-16 14:57:36 -0500

    Updating Milestone 11. Was having some issues with the filteredHousingList Variable. After talking with my teacher and brainstorming, we came up with a solution that makes everything work. You'll notice that the if statement and housingLocationList variable are commented out in the home.ts file. This makes the application work properly.

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -11,12 +11,24 @@ import { HousingService } from '../housing-service';
 })
 export class Home {
   // readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-  housingLocationList: HousingLocationInfo[] = [];
+  // housingLocationList: HousingLocationInfo[] = [];
   // Injecting HousingService into the Component
   housingService: HousingService = inject(HousingService);
 
+  filteredLocationList: HousingLocationInfo[] = [];
+
   constructor() {
     // setting housingLocationList to the data array in HousingService
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingService.getAllHousingLocations();
+  }
+
+  filterResults(text: string) {
+    // if (!text) {
+    //   this.filteredLocationList = this.housingLocationList;
+    //   return;
+    // }
+    this.filteredLocationList = this.filteredLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
   }
 }
